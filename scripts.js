9f1bf186cdb5dc4666a0759b12c907a7c9a5a806
atsnokoto <80885477+atsnokoto@users.noreply.github.com>
2022-01-10 11:14:55 +0200

    referer

--- scripts.js
+++ scripts.js
@@ -10,6 +10,8 @@ if(window.innerWidth < 800){
     document.location = "indexMobile.html";
 }
 
+var whereUserCameFrom = document.referrer;
+
 
 /// TEHDÄÄN KUN SIVUSTO AVATAAN: 
 // Asetetaan kokonais klikkien ja boostien määrät:
@@ -1183,6 +1185,7 @@ function levelUpListener(){
     if (totalEnergy === 50){
 
         //location.replace("https://www.w3schools.com")
+
         window.location.replace("https://dev81602.service-now.com/tf","_self")
 
     }
